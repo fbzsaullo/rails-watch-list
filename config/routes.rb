c59cfce676e11c6ccd1203f8a5baa5baaa4cec9e
Fabrizio Saullo <fabriziodsaullo@gmail.com>
2022-05-26 14:45:41 -0300

    controller list and bookmark

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, except: %i[destroy edit update] do
+    resources :bookmarks, except: %i[edit update destroy]
+  end
+
+  resources :bookmarks, :destroy
 end
